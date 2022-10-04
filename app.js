d11f804540a7920de573533a6d3cc59908aa03da
jasonygn <kzyangon@protonmail.com>
2022-10-04 16:27:42 +0630

    add
    add

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Post = require('./models/Post');
+
+const DB_CONNECTION = "mongodb+srv://:@cluster0.kz00hur.mongodb.net/db_donor"; 
+const PORT = 4000; 
+
+
+//app.use(express.static(__dirname + '/views/style.css'));
+
+app.use(cors());
+app.use(bodyParser.json()); 
+//we can do app.use(auth)
+
+app.set('view engine', 'ejs');
+
+const postsRoute = require('./routes/posts');
+//const { mongo } = require('mongoose');
+
+app.use('/posts', postsRoute);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/home.html");   
+})
+
+app.get('/view', (req, res) => {
+    Post.find({}, function(err, posts) {
+        res.render('index', {
+            postsList: posts 
+        })
+    })
+})
+
+app.get('/tview', (req, res) => {
+    Post.find({}, function(err, posts) {
+        res.render('index2', {
+            postsList: posts 
+        })
+    })
+})
+
+mongoose.connect(DB_CONNECTION, () =>
+    console.log('MongoDB Connection Successful!')
+);
+
+app.listen(PORT);
+console.log(`Server is listening on: ${PORT}`);    
+
+
